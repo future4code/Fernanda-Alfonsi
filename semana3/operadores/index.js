f6bd21507916d0945927c43bb22c59339b06d671
fefealfonsi <fefealfonsi@gmail.com>
2020-09-29 16:50:52 -0300

    Comecei a lição mas não terminei. falta exercício 2,3 e 4

---
+++ semana3/operadores/index.js
@@ -0,0 +1,26 @@
+// Exercício de Interpretação
+
+// EXERCÍCIO 1
+
+// No console aparecerá:
+// a. false 
+// b. false
+// c. true
+// e. boolean
+
+
+// ECERCÍCIO 2
+
+// a. undefined
+// b.null
+// c. 11
+// d. 3
+// e. [3, 19, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+// f. 9
+
+// Exercício de escrita
+
+const idadeDoUsuario = prompt('Qual sua idade?')
+const idadeDoAmigo = prompt('Qual a idade do seu ou sua melhor amigue?')
+const comparar = cast.number(idadeDoUsuario)> cast.number(idadeDoAmigo)
+console.log ('sua idade é maior que a do seu melhor amigue?', comparar)
