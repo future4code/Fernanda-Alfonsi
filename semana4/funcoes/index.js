7a39b875c57d2dcf0cf2386f1e0d054e589fb3eb
fefealfonsi <fefealfonsi@gmail.com>
2020-10-05 17:02:51 -0300

    Exercício incompleto, falta 6 e os desafios

---
+++ semana4/funcoes/index.js
@@ -0,0 +1,91 @@
+// **Exercícios de interpretação de código**
+// Exercício 1
+// a. Será impresso no console os valores 10 referente a 2*5 e 50 referente a 10*5
+// b. as respostas estariam guardadas nas funções, mas não imprimiria nada no console.
+
+//  Exercício 2
+// a. seria impresso no console  os dois primeiros nomes da array "Darvas e Caio"
+// será impresso os nomes de Amanda e Caio, nesta sequência.
+
+
+// Exercício 3
+//  a função metodo percorre os objetos da array e busca entre eles quais são pares, assim que detectados o programa faz uma nova lista "arrayFinal" apenas com os quadrados dos pares  e retorna esta lista. Um nome melhor pra esta função seria "quadradoDoPares. ""
+
+// **Exercícios de escrita de código**
+// Exercício 4
+// A.
+
+// function oiSouEu(){
+//     console.log(`Oie! Meu nome é Fernanda, tenho 36 anos, moro em São Paulo minha vida inteira e, neste momento, sou estudante`)
+// }
+// oiSouEu()
+
+
+// index.js:19 Oie! Meu nome é Fernanda, tenho 36 anos, moro em São Paulo minha vida inteira e, neste momento, sou estudante
+
+// B.
+
+// let nome = prompt("Qual seu nome?")
+// let idade = Number(prompt("Qual sua idade?"))
+// let cidade = prompt("Qual sua Cidade?")
+// let estudante = prompt("Você é estudante? Responda sim ou não")
+
+// let meusDados=(nome, idade, cidade, estudante)=>{
+//     if (estudante==="sim"){
+//         estudante ="sou" 
+
+//     }else{
+//         estudante = "não sou"
+//     }
+
+//     console.log(`Oie! Meu nome é ${nome}, tenho ${idade} anos, moro em ${cidade} minha vida inteira e, neste momento, ${estudante} estudante`)
+// }
+// const resultado=meusDados(nome, idade, cidade, estudante)
+
+
+
+// index.js:41 Oie! Meu nome é Fernanda, tenho 36 anos, moro em São Paulo minha vida inteira e, neste momento, sou estudante
+
+// Exercício 5
+// // A.
+// let doisNumeros=(a,b)=>{
+//     const soma = Number(a+b)
+//     console.log(soma)
+// }
+// doisNumeros(10, 205)
+
+
+// index.js:53 215
+
+
+// B.
+
+// let compararNumeros=(a,b)=>{
+//     if(a<b){
+//         resultado = console.log(`O numero ${a} é menor que ${b}`)
+//     }else if (a===b){
+//         resultado = console.log(`O numero ${a} é igual a ${b}`)
+//     }else{
+//         resultado = console.log(`O numero ${a} é maior que ${b}`)
+//     }
+// }
+//  let resultado = compararNumeros(5, 3)
+
+
+//  index.js:65 O numero 3 é menor que 209
+
+// index.js:65 O numero 3 é menor que 209
+
+// index.js:69 O numero 5 é maior que 3
+// C.
+
+// function dezXMensagem(texto){
+//     for(let i=0; i<10; i++){
+//         console.log(texto)
+//     }
+// }
+// let mensagem = dezXMensagem("oi gente")
+
+
+// 10
+// index.js:70 oi gente
