c927dca56528944ce46c00a6829e9da4f7c24408
fefealfonsi <fefealfonsi@gmail.com>
2020-10-12 16:02:53 -0300

    Exercício 5 de Funções de Array

--- semana4/lista-de-exercícios/index.js
+++ semana4/lista-de-exercícios/index.js
@@ -426,3 +426,26 @@ const mandaEmail=(array) =>{
    }
    mandaEmail(consultas)
    **/
+
+   /** Exercício 5
+
+const contas = [
+	{ cliente: "João", saldoTotal: 1000, compras: [100, 200, 300] },
+	{ cliente: "Paula", saldoTotal: 7500, compras: [200, 1040] },
+	{ cliente: "Pedro", saldoTotal: 10000, compras: [5140, 6100, 100, 2000] },
+	{ cliente: "Luciano", saldoTotal: 100, compras: [100, 200, 1700] },
+	{ cliente: "Artur", saldoTotal: 1800, compras: [200, 300] },
+	{ cliente: "Soter", saldoTotal: 1200, compras: [] }
+]
+
+contas.forEach((cliente, i, array)=>{
+    let total=0
+    
+    for(numero of (cliente.compras)){
+        total= total+numero
+    }
+    cliente.saldoTotal= cliente.saldoTotal- total
+    
+})
+console.log(contas)
+**/
