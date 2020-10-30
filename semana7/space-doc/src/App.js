4fd766c7a843cff80a81f66fbfbea3532f4c5632
fefealfonsi <fefealfonsi@gmail.com>
2020-10-29 22:38:43 -0300

    Implementação da minha App feita, incompleta

--- semana7/space-doc/src/App.js
+++ semana7/space-doc/src/App.js
@@ -5,45 +5,64 @@ import axios from "axios"
 
 const ImagemSatelite =styled.img`
   width: 7em;
-  margin: 0em 6em;
+  margin-right: 10em;
+  
 `
+const DivPrincipal =styled.div`
+  display:flex;
+  flex-direction: column;
+  align-items:center;
 
+`
 
 
 class App extends React.Component{
 
   state={
     listaMissoes: [],
-    selectHistoria:""
+    detalheDaMissao:""
 
+  };
+  componentDidMount() {
+    this.buscarMissoes();
+    
   }
-  onChangeSelect = (event) => {
-    console.log("SELECIONOU ALGO", event.target.value);
-    const apiUrl = `https://api.spacexdata.com/v3/history/${event.target.value}`;
-    axios.get(apiUrl).then((response) => {
-     
-      this.setState({
-        selectHistoria: response.data.description
+  
+
+  buscarMissoes = () => {
+    axios.get("https://api.spacexdata.com/v3/history").then((response) => {
+      this.setState({listaMissoes: response.data });
+    });
+  };
+  onClickSelect = (missaoId) => {
+    axios.get(`https://api.spacexdata.com/v3/history/${missaoId}`).then((resposta) => {   this.setState({
+        detalheDaMissao: resposta.data.details
+
+
       });
     });
   };
+  
   render(){
-    console.log(this.state.listaMissoes);
-    const optionList = this.state.listaMissoes.map((missao) => {
-      return <option key={missao.id}>{missao.title}</option>;
+    
+    const listaOpçoes = this.state.listaMissoes.map((missao) => {
+      return <option onClick={()=>this.onChangeSelect(missao.id)} key={missao.id}>{missao.title}</option>;
+
+      
     });
     return (<div>
       <header>
       <h1><ImagemSatelite src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSV8yIOFmNCvTAb3Nog9esbmcL1Kk7cVlEXZA&usqp=CAU"/> Space history</h1>
        </header>
-      <div>
+      <DivPrincipal>
         <h3>Escolha uma missão e conheça sua história. </h3>
-        <select on onChange={this.onChangeSelect}>
-          {optionList}
+        <select>
+          {listaOpçoes}
         </select>
         <hr></hr>
-        <h3> <ImagemSatelite src="https://e7.pngegg.com/pngimages/868/698/png-clipart-graphics-drawing-design-satellite-signals-purple-cartoon.png"/>História:</h3>
-      </div>
+        <h3>Detalhes da missão:</h3>
+        <p> {this.state.detalheDaMissao}</p>
+      </DivPrincipal>
     </div>
 
     
