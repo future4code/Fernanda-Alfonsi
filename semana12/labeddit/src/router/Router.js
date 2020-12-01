c94c91a2c4ac5ac33fa11f74bbd87faff5e20186
fefealfonsi <fefealfonsi@gmail.com>
2020-12-01 14:03:48 -0300

    Apenas um comit para testar uma hipotese

---
+++ semana12/labeddit/src/router/Router.js
@@ -0,0 +1,44 @@
+import React from 'react'
+
+import FeedPage from '../pages/FeedPage'
+import LoginPage from '../pages/LoginPage'
+import RegisterFormPage from '../pages/RegisterFormPage'
+import PostPage from '../pages/PostPage'
+import ErrorPage from '../pages/ErrorPage'
+
+import { BrowserRouter, Route, Switch } from "react-router-dom"
+
+function Router() {
+  return (
+    <BrowserRouter>
+    <Switch>
+      
+      <Route exact path="/">
+        <FeedPage/>
+      </Route>
+
+      <Route exact path="login">
+        <LoginPage/>
+      </Route>
+
+      <Route exact path="/register/:id">
+        <RegisterFormPage/>
+      </Route>
+
+      <Route exact path="/post">
+        <PostPage/>
+      </Route>
+
+     
+      <Route path="/error">
+        <ErrorPage/>
+      </Route>
+
+    </Switch >
+  </BrowserRouter>
+  
+
+  );
+}
+
+export default Router;
