42c71213aef3eeb45c5121c96b0c295afb970135
fefealfonsi <fefealfonsi@gmail.com>
2021-01-05 17:02:34 -0300

    exercícios semana 15 aula 2

---
+++ semana15/aula2/Exercícios semana15 -aula2/first-script/src/exercicio3.ts
@@ -0,0 +1,42 @@
+// Parte A
+
+type post = {
+    autor:string,
+    texto:string,
+}
+
+const posts: post[] = [
+    {
+      autor: "Alvo Dumbledore",
+      texto: "Não vale a pena viver sonhando e se esquecer de viver"
+    },
+    {
+      autor: "Severo Snape",
+      texto: "Menos 10 pontos para Grifinória!"
+    },
+    {
+      autor: "Hermione Granger",
+      texto: "É levi-ô-sa, não levio-sá!"
+    },
+    {
+      autor: "Dobby",
+      texto: "Dobby é um elfo livre!"
+    },
+    {
+      autor: "Lord Voldemort",
+      texto: "Avada Kedavra!"
+    }
+  ]
+
+//   Parte B
+// as entradas 
+
+function buscarPostsPorAutor(posts, autorInformado) {
+    return posts.filter(
+      (post) => {
+        return post.autor === autorInformado
+      }
+    )
+  }
+
+//   Quais são as entradas e saídas dessa função? Copie a função para o mesmo arquivo .ts do array de posts e faça a tipagem necessária. 
---
+++ semana15/aula2/Exercícios semana15 -aula2/first-script/src/index.ts
@@ -0,0 +1,1 @@
+console.log("Eita Mundão")
